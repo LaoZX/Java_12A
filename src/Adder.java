ada335799096c6f70aad445e6851a4a647343548
LaoZX <109078028+LaoZX@users.noreply.github.com>
2023-03-31 14:46:59 +0900

    Update Adder.java

--- src/Adder.java
+++ src/Adder.java
@@ -1,4 +1,4 @@
-public class Adder {//run with terminal"$Java Adder 12 3 6"
+public class Adder {//run with terminal"$Java Adder 12 3 6" in src folder
     public static void main(String[] args){
         int sum=0;
         for(String str: args){
